19076f80c3433d18af94fe58a0d172388231cd0b
Kira McDonald <kira.mcd.99@gmail.com>
2023-06-15 10:58:05 -0400

    added recessions helper and plot templates

---
+++ R/plot-themes-and-palettes.R
@@ -0,0 +1,94 @@
+#' add.plot.theme
+#'
+#' Add base plot theme.
+#'
+#' @export add.plot.theme
+add.plot.theme <- function( fontsize = 10
+                            ,leg.pos = 'top'
+                            #,font='Arial'
+                            #,facet.label.font.size = fontsize
+                            ,...
+) {
+  require(extrafont)
+
+  list(
+    theme_minimal() ,
+    theme( panel.grid.major.x = element_blank()
+           ,legend.position = leg.pos
+           #,panel.grid.major.x = element_blank()
+           ,panel.grid.minor = element_blank()
+           ,text = element_text(
+             size = fontsize,
+             family = font)
+           # for facet warp labels
+           ,strip.background = element_rect(
+             fill = aevis::aecom.palette()[2] )
+           ,strip.text = element_text(
+             face = 'bold'
+             ,color = 'white'
+             ,size = fontsize - 1)
+           ,... )
+  )
+}
+
+
+#' add.map.theme
+#'
+#' Add base map theme.
+#'
+#' @export add.map.theme
+add.map.theme <- function(fontsize = 11
+                          ,leg.pos = 'right'
+                          #,font='Arial'
+                          ,lmargin = margin(4,4,4,4)
+                          , ...) {
+  require(extrafont)
+
+  list(
+    theme_void() ,
+    theme( legend.position = leg.pos
+           ,text = element_text( size = fontsize
+                                 ,family=font)
+           ,legend.box.margin = lmargin
+           ,legend.justification = 'top'
+           ,... )
+  )
+}
+
+
+#' upper.legend.box
+#'
+#' Draws a box around a legend and top-justifies
+#'
+#' @export upper.legend.box
+upper.legend.box <- function() {
+
+  theme(
+    legend.box.background = element_rect(color="gray30"
+                                         , size=.05)
+    ,legend.box.margin = margin(4,4,4,4)
+    ,legend.justification = 'top'
+  )
+}
+
+
+
+
+
+# data tables -------------------------------------------------------------
+
+
+#' dt.table.template
+#'
+#' Wrapped DT::datatable defaults
+#'
+#' @export dt.table.template
+dt.table.template <- function(x) {
+
+  DT::datatable(x
+                ,extensions = 'Buttons'
+                ,options = list(dom='Bfrtp'
+                                ,ordering=F
+                                ,pageLength =20
+                                ,buttons=c('copy','csv','excel')))
+}
--- R/times-series-plotting.R
+++ R/times-series-plotting.R
@@ -35,3 +35,82 @@ ts.labels.geom <- function(tsx,
   )
   return(lbls)
 }
+
+
+
+# recessions for time series ----------------------------------------------
+
+
+#' get.recessions.start.ends
+#'
+#' Gets recession starts and endpoints; queries from FRED series "JHDUSRGDPBR"
+#' and sets up a tibble w start and end dates as two columns.
+#'
+#' @param recessions as pulled from `fredr::fredr(series_id = 'JHDUSRGDPBR')`,
+#'   with additional arguments for month/qtr/annual or observation start.
+#'
+#' @export get.recessions.start.ends
+get.recessions.start.ends <- function(recessions = NULL) {
+
+
+  if(is.null(recessions))
+    recessions <- fredr::fredr(
+      series_id = 'JHDUSRGDPBR') %>%
+      select(-matches('^realtime'))
+
+  recessions <- recessions %>%
+    mutate(start =
+             lag(value, order_by = date) %in% c(0, NA) &
+             value == 1
+           ,end =
+             lead(value, order_by = date) %in% c(0, NA) &
+             value == 1
+    )
+
+  recessions <- tibble(
+    starts = filter(recessions, start)$date
+    ,ends = filter(recessions, end)$date
+  )
+
+  return(recessions)
+}
+
+
+
+#' gg.recessions.ts
+#'
+#' Adds colored bars to a (time-series) ggplot, as to indicate recessions. By
+#' default queries recessions from FRED series "JHDUSRGDPBR."
+#'
+#' @param recessions A time series of recessions (or other periodizations) to
+#'   add to ggplot. Expects a `date` column.
+#' @param rcolor color for bars indicating recessions etc.
+#' @param ... passed onto `fredr::fredr`; months or quarters and obs start may
+#'   be helpful.
+#'
+#' @export gg.recessions.ts
+gg.recessions.ts <- function(recessions = NULL
+                             ,rcolor = 'grey90'
+) {
+
+  if(is.null(recessions))
+    recessions <- get.recessions.start.ends()
+
+  lyr <- geom_rect(
+    data = recessions
+    ,aes(
+      xmin = starts
+      ,xmax = ends
+      ,ymin = -Inf
+      ,ymax = Inf
+    )
+    #,ymin = -Inf
+    ,fill = rcolor
+    ,color = rcolor
+    ,inherit.aes = F
+    ,show.legend = F
+  )
+
+  return(lyr)
+}
+
